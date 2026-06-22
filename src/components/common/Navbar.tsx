fd7ce6098d3d92c1645e0ce9cd20ccf33f549296
mon <mohansp119@gmail.com>
2026-06-23 00:06:34 +0530

    Navbar seatup done.

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,10 +1,28 @@
+import { navItems } from "@/config/common";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex justify-between items-center w-6xl mx-auto bg-primary sticky top-2 rounded-xl px-4 py-1">
+      <div className="">
+        <Image src={"/assets/logo.png"} alt="Logo" width={50} height={50} />
+      </div>
+      <div className="flex gap-3 items-center text-white">
+        <ul className="flex gap-3 items-center">
+          {navItems.map((item) => (
+            <Link className="" href={item.link} key={item.id}>
+              {item.title}
+            </Link>
+          ))}
+        </ul>
 
-export {Navbar}
+        <button className="bg-white text-primary px-3 py-1 rounded-lg border border-secondary">
+          Book Appointment
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { Navbar };
---
+++ src/config/common.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    { id: 1, title: "Home", link: '/' },
+    { id: 2, title: "About", link: '/about' },
+    { id: 3, title: "Services", link: '/services' },
+    { id: 4, title: "Testimonials", link: '/testimonials' },
+]
